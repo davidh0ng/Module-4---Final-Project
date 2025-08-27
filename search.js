9cbd5e3863165e4e3f2330efa0efdfcd4cd65cbb
davidh0ng <djhong83@gmail.com>
2025-08-27 09:57:10 -0700

    Updated a template for movie results to set up for javascript results

--- search.js
+++ search.js
@@ -1,29 +1,30 @@
-const range1 = document.getElementById("minRange");
-const range2 = document.getElementById("maxRange");
-const highlight = document.querySelector(".search__slider--range");
-const output = document.getElementById("output");
+console.log("test")
 
-console.log(range1.value);
-console.log(range2.value);
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('.search__input');
+    input.addEventListener('keyup', async (e) => {
+        const userInput = e.target.value.trim();
+        if (userInput.length > 0) {
+            await movieSearch(userInput);
+        }
+    });
+});
 
-function updateHighlight () {
-    let min = parseInt(range1.value);
-    let max = parseInt(range2.value);
+let movies = [];
 
-    if (min > max) {
-        [min, max] = [max, min];
+async function movieSearch(userInput) {
+    const getMovies = await fetch(`https://omdbapi.com/?s=${userInput}&page=1&apikey=5c395d5b`);
+    const movieList = await getMovies.json();
+
+    if (movieList.Response === 'False') {
+        return;
     }
-
-    console.log(min);
-    console.log(max);
-
-    highlight.style.left = (min / 100 *300) + "px";
-    highlight.style.width = ((max - min) / 100 * 300) + "px";
-
-    output.textContent = min + " - " + max;
 }
 
-range1.addEventListener("input", updateHighlight);
-range2.addEventListener("input", updateHighlight);
 
-updateHighlight();
+
+
+async function filterMovies() {
+    const getMovie = await fetch('https://www.omdbapi.com/?s=inception&apikey=5c395d5b');
+    const data = await getMovie.json();
+}
