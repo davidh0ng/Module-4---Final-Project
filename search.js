0ab6cd2c9a04f7de75e0e811c1231fc8d0e90254
davidh0ng <djhong83@gmail.com>
2025-08-30 20:39:51 -0700

    Made some final changes to the browsing function and styling

--- search.js
+++ search.js
@@ -1,30 +1,32 @@
-console.log("test")
+console.log("test");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const input = document.querySelector('.search__input');
-    input.addEventListener('keyup', async (e) => {
-        const userInput = e.target.value.trim();
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector(".search__input");
+  input.addEventListener("keyup", async (e) => {
+    const userInput = e.target.value.trim();
         if (userInput.length > 0) {
             await movieSearchResult(userInput);
-        }
-    });
+        };
+  });
 });
 
 async function movieSearchResult(userInput) {
-    const movieWrapper = document.querySelector(".search__results--wrapper");
+  const movieWrapper = document.querySelector(".search__results--wrapper");
 
-    const getMovies = await fetch(`https://omdbapi.com/?s=${userInput}&page=1&apikey=5c395d5b`);
-    const movieList = await getMovies.json();
+  const getMovies = await fetch(
+    `https://omdbapi.com/?s=${userInput}&page=1&apikey=5c395d5b`
+  );
+  const movieList = await getMovies.json();
 
-    if (movieList.Response === 'False') {
-        return;
-    }
+  if (movieList.Response === "False") {
+    return;
+  }
 
-    const movieArray = movieList.Search;
-    
-    const movieListHTML = movieArray.map(movie => {
-  
-        return `<div class="search__result--box">
+  const movieArray = movieList.Search;
+
+  const movieListHTML = movieArray
+    .map((movie) => {
+      return `<div class="search__result--box">
                 <figure class="search__result--img">
                   <img
                     src="${movie.Poster}"
@@ -33,13 +35,9 @@ async function movieSearchResult(userInput) {
                 </figure>
                 <h1 class="search__result--title">${movie.Title}</h1>
                 <h2 class="search__result--year">${movie.Year}</h2>
-              </div>`
-    }).join("");
+              </div>`;
+    })
+    .join("");
 
-    movieWrapper.innerHTML = movieListHTML;
-}
-
-async function filterMovies() {
-    const getMovie = await fetch('https://www.omdbapi.com/?s=inception&apikey=5c395d5b');
-    const data = await getMovie.json();
+  movieWrapper.innerHTML = movieListHTML;
 }
